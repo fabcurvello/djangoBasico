d01023d0eec78e38911cd8a584358c6fdafe7911
fabriciocurvello <tiofab@gmail.com>
2023-07-28 15:14:34 -0300

    Insercao de produtos via template

--- produtos/views.py
+++ produtos/views.py
@@ -3,7 +3,15 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index_produtos(request):
-    return render(request, 'produtos/index.html')
+    context = {
+        'produto': 'Tablet iPad', 'preco': '4550.00',
+        'produtos': [
+            {'produto': 'Smart TV LG 70 Polegadas', 'preco': '5250.00'},
+            {'produto': 'Smart TV Samsung 50 Polegadas', 'preco': '3000.00'},
+            {'produto': 'Smart TV Philips 42 Polegadas', 'preco': '2100.00'},
+        ]
+    }
+    return render(request, 'produtos/index.html', context)
 
 def celulares(request):
     return HttpResponse('Página inicial de Celulares')
