c4ecb452351f40360c14eec4d1378bc67f46c553
Fabricio Curvello <fabricio.desenvolvedor@gmail.com>
2023-08-04 12:42:28 -0300

    Models.py com migrations incluídas e aplicadas (cliente)

--- clientes/models.py
+++ clientes/models.py
@@ -1,4 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Cliente(models.Model):
+    nome = models.CharField('Nome', max_length=30)
+    email = models.EmailField('E-mail', max_length=40)
+    nascimento = models.DateField('Data de Nascimento')
 
