ac3a9138c89417e8883e0a9705f65d33f6918745
Fabricio Curvello <fabricio.desenvolvedor@gmail.com>
2023-08-04 14:30:54 -0300

    Adaptação do bloco de aplicações instaladas no settings.py do projeto

--- djangobasico/settings.py
+++ djangobasico/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'produtos',
+    'produtos.apps.ProdutosConfig',
     'clientes.apps.ClientesConfig',
     'inicio.apps.InicioConfig',
 ]
