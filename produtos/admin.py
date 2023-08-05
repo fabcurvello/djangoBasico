8d8370cc15cc80866facf2a37164e2b84054a0b5
Fabricio Curvello <fabricio.desenvolvedor@gmail.com>
2023-08-04 21:05:55 -0300

    Área administrativa de produto concluída

--- produtos/admin.py
+++ produtos/admin.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 from .models import Produto
 
 # Register your models here.
-admin.site.register(Produto)
+class ProdutoAdmin(admin.ModelAdmin):
+    list_display = ('nomenclatura', 'preco', 'estoque')
+
+
+admin.site.register(Produto, ProdutoAdmin)
 
 
