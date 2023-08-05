f6f06b8f1df4eacd5eb4c9acfab228e1659418af
Fabricio Curvello <fabricio.desenvolvedor@gmail.com>
2023-08-05 11:55:01 -0300

    Área administrativa de cliente em andamento

--- clientes/admin.py
+++ clientes/admin.py
@@ -1,17 +1,12 @@
 from django.contrib import admin
 from .models import Cliente
 
-# Register your models here.
+
 class ClienteAdmin(admin.ModelAdmin):
     list_display = ('nome', 'nascimento', 'email')
 
 
 admin.site.register(Cliente, ClienteAdmin)
-
-
-
-
-
 
 
 
