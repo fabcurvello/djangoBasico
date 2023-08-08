0c05f0ce24ba6f00a0172182ff3c014388a7cf95
Fabricio Curvello <fabricio.desenvolvedor@gmail.com>
2023-08-07 21:29:56 -0300

    Lista de clientes do DB para o HTML - ETAPA 1 - views.py da app clientes

--- clientes/views.py
+++ clientes/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
+from .models import Cliente
 
 # Create your views here.
 def index_clientes(request):
-    return render(request, 'clientes/index.html')
+    clientes_db = Cliente.objects.all()
+    context = {
+        'lista_clientes_db': clientes_db,
+    }
+    return render(request, 'clientes/index.html', context)
 
 
 
