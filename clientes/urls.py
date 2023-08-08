0cf5a93286d32f514f2942c3477986182be71d62
Fabricio Curvello <fabricio.desenvolvedor@gmail.com>
2023-08-07 22:39:53 -0300

    Tornar cliente exibido no HTML um item clicável e que direciona para html específico do cliente selecionado. ETAPA 3 - modificar urls.py da app clientes

--- clientes/urls.py
+++ clientes/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+from .views import cliente_id
 
 urlpatterns = [
-    path('', views.index_clientes),
+    path('', views.index_clientes, name='index_clientes'),
+    path('<int:id>', cliente_id, name='cliente_id'),
 ]
 
 
